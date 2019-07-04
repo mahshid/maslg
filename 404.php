4255db086e78ce14b24e5b7834609fb43834fe7a
Misagh <misagh1663@yahoo.com>
2019-07-04 09:32:54 +0430

    Add robots.txt file and 404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php $no_title = true; ?>
+<?php $title = '404 Not Found'; ?>
+<?php require_once './template/header.php'; ?>
+<?php require_once './template/content.php'; ?>
+
+<div class="col-12 text-center mb-5">
+	<h1>Not Found</h1>
+	<hr class="title-hr">
+</div>
+<div class="col-12 text-center">
+	<p>Sorry! The requested URL was not found on this site.</p>
+	<p><a href="/">Back to Home</a></p>
+</div>
+
+<?php require_once './template/footer.php'; ?>
--- template/header.php
+++ template/header.php
@@ -3,11 +3,18 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Misagh Laghaei | میثاق لقایی<?php if (! empty($title)) echo ' | ' . $title; ?></title>
+	<title><?php if (! empty($title)) echo $title . (empty($no_title) ? ' | ' : ''); ?><?php if (empty($no_title)) echo 'Misagh Laghaei | میثاق لقایی'; ?></title>
 	<?php if (! empty($meta_description)) { ?>
 	<meta name="description" content="<?php echo $meta_description; ?>">
 	<?php } ?>
-	<link rel="icon" type="image/png" href="includes/favicon-256.png">
+	<meta property="og:title" content="<?php if (! empty($title)) echo $title . (empty($no_title) ? ' | ' : ''); ?><?php if (empty($no_title)) echo 'Misagh Laghaei | میثاق لقایی'; ?>">
+	<?php if (! empty($meta_description)) { ?>
+	<meta property="og:description" content="<?php echo $meta_description; ?>">
+	<?php } ?>
+	<meta property="og:url" content="https://<?php echo str_replace('www.', '', $_SERVER['HTTP_HOST']) . $_SERVER['REQUEST_URI']; ?>">
+	<meta property="og:image" content="/includes/misagh_laghaei.jpg">
+	<link rel="shortcut icon" href="/includes/favicon-256.png">
+	<link rel="icon" sizes="16x16 32x32" type="image/png" href="/includes/favicon-256.png">
 	<link href="https://fonts.googleapis.com/css?family=Chicle" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Finger+Paint" rel="stylesheet">
